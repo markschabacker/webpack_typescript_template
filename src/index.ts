37c79cc45a581d0a522900433f4e8449e9d2695a
Mark Schabacker <mark.schabacker@agilethought.com>
2017-06-01 08:50:13 -0400

    Make template script more informative

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-alert('alert from index.ts');
+window.addEventListener('load', () => {
+    document.getElementById('load-target').innerHTML = `Webpack Generated Scripts Loaded at ${new Date()}`;
+});
